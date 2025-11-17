0230952ca7b7016b94f1c950dd4617d7c92a1954
osman.korkmaz <osman.korkmaz@vbt.com.tr>
2025-11-17 20:19:01 +0300

    refactor: standardize string quotes and update navigation path in delete ticket type button component

--- apps/web/src/features/ticket-types/ui/components/delete-ticket-type-button.tsx
+++ apps/web/src/features/ticket-types/ui/components/delete-ticket-type-button.tsx
@@ -1,9 +1,9 @@
-import { useMutation } from "@tanstack/react-query";
-import { deleteTicketTypeMutation } from "../../lib/mutations";
-import { Button } from "@/components/ui/button";
-import { TrashIcon } from "lucide-react";
-import useConfirm from "@/hooks/use-confirm";
-import { redirect, useNavigate } from "@tanstack/react-router";
+import { useMutation } from '@tanstack/react-query';
+import { deleteTicketTypeMutation } from '../../lib/mutations';
+import { Button } from '@/components/ui/button';
+import { TrashIcon } from 'lucide-react';
+import useConfirm from '@/hooks/use-confirm';
+import { redirect, useNavigate } from '@tanstack/react-router';
 
 interface DeleteTicketTypeButtonProps {
   id: string;
@@ -15,9 +15,9 @@ export default function DeleteTicketTypeButton({
   const navigate = useNavigate();
   const deleteTicketType = useMutation(deleteTicketTypeMutation);
   const [DeleteConfirmDialog, confirmDelete] = useConfirm(
-    "Silme Onayı",
-    "Bu bilet türünü silmek istediğinize emin misiniz?",
-    "Sil"
+    'Silme Onayı',
+    'Bu bilet türünü silmek istediğinize emin misiniz?',
+    'Sil',
   );
   const handleDelete = async () => {
     const confirmed = await confirmDelete();
@@ -26,17 +26,17 @@ export default function DeleteTicketTypeButton({
         { ticketTypeId: id },
         {
           onSuccess: () => {
-            navigate({ to: "/settings/ticket-types", replace: true });
+            navigate({ to: '/admin-settings/ticket-types', replace: true });
           },
-        }
+        },
       );
     }
   };
   return (
     <>
       <DeleteConfirmDialog />
-      <Button variant="destructive" onClick={handleDelete}>
-        <TrashIcon className="size-4" />
+      <Button variant='destructive' onClick={handleDelete}>
+        <TrashIcon className='size-4' />
         Sil
       </Button>
     </>
