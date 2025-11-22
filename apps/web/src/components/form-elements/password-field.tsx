f4b65dee8019bd5aad2ee7418da1aa909a88e46f
osman.korkmaz <osman.korkmaz@vbt.com.tr>
2025-11-22 19:40:23 +0300

    fix: change input type from 'email' to default in PasswordField component for correct password handling

--- apps/web/src/components/form-elements/password-field.tsx
+++ apps/web/src/components/form-elements/password-field.tsx
@@ -32,7 +32,6 @@ export const PasswordField = ({
         onBlur={field.handleBlur}
         name={field.name}
         id={field.name}
-        type='email'
         {...props}
       />
     </FormField>
